cda29802a3cca25ea4ea6798749dc96163851df1
Kevin S <kevinolisiin@gmail.com>
2021-04-22 14:14:31 +0300

    new gatsby project

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+	return (
+		<main>
+			<title>Contact page</title>
+			<h1>Let's get in touch</h1>
+		</main>
+	);
+};
+
+export default Contact;
---
+++ src/pages/projects/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+	return (
+		<main>
+			<title>All projects</title>
+			<h1>Done work</h1>
+		</main>
+	);
+};
+
+export default Projects;
