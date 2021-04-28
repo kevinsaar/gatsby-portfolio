f74b8f418ce3e1dba4583e757d1156971e66b4cd
Kevin S <kevinolisiin@gmail.com>
2021-04-28 23:19:59 +0300

    cv link

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,38 @@
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 import './About.scss';
 import SkillsSVG from './SkillsSVG';
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
 
 const About = () => {
+	const data = useStaticQuery(graphql`
+		query AboutSection {
+			allContentfulHomepage {
+				nodes {
+					aboutSectionTitle {
+						normal
+						colorful
+					}
+					aboutMeContent {
+						raw
+					}
+					cv {
+						title
+						file {
+							url
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	const {
+		aboutSectionTitle,
+		aboutMeContent,
+		cv,
+	} = data.allContentfulHomepage.nodes[0];
+
 	return (
 		<section className='about'>
 			<div className='max-width'>
@@ -15,37 +45,15 @@ const About = () => {
 						<span>T</span>
 					</span>
 					<SkillsSVG />
-					<div className='text'>
-						<h2>
-							About <b>me</b>
-						</h2>
-						<p>
-							I'm a front-end web developer with{' '}
-							<b>1 year of work experience</b> based in Tallinn, Estonia.
-						</p>
-						<p>
-							Strongest weapons I have in my arsenal right now are <b>SCSS</b>{' '}
-							and <b>HTML</b> sprinkled with some JavaScript. Nowadays you can't
-							be seen without <b>ReactJS</b> so I'm slowly adding that in as
-							well. Honorable mentions include WordPress, Git, GitHub and npm.
-						</p>
-						<ul className='skills'>
-							<li>HTML</li>
-							<li>SCSS</li>
-							<li>ReactJS</li>
-							<li>JavaScript</li>
-							<li>WordPress</li>
-							<li>Git</li>
-							<li>GitHub</li>
-							<li>NPM</li>
-						</ul>
-						<p>
-							At the moment <b>looking for working opportunities</b>. If you
-							might be interested have a look at my CV or contact by any means.
-						</p>
-						<a href='/' target='_blank' rel='noreferrer'>
-							Look at CV
-						</a>
+					<div className='content'>
+						{aboutSectionTitle && (
+							<h2>
+								{aboutSectionTitle.normal}
+								<b> {aboutSectionTitle.colorful}</b>
+							</h2>
+						)}
+						{aboutMeContent && renderRichText(aboutMeContent)}
+						{cv && <Link to={cv.file.url}>{cv.title}</Link>}
 					</div>
 				</div>
 			</div>
